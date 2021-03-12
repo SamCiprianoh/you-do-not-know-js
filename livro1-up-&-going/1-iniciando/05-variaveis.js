d9974729297a0a23f0365e219c06a6e91f52958e
Samuel Cipriano <samciprianoh@gmail.com>
2021-03-12 19:46:32 -0300

    adicionado informaçao extra em variaveis

--- livro1-up-&-going/1-iniciando/05-variaveis.js
+++ livro1-up-&-going/1-iniciando/05-variaveis.js
@@ -1,3 +1,7 @@
+//O primeiro propósito das variáveis: gerenciar o estado do programa.
+//Em outras palavras, estado é o acompanhamento das mudanças dos valores conforme seu programa está rodando.
+
+
 //O JavaScript usa a tipagem dinâmica, o que significa que as variáveis podem armazenar valores de qualquer tipo.
 
 //Declaramos uma variável usando a instrução var -- note que não existe aqui nenhuma informação sobre tipo nessa declaração
@@ -25,3 +29,4 @@ const a = 5
 //Você declara essas constantes geralmente no início do programa, de forma a se tornar um lugar conveniente de se visitar caso deseje alterar algum valor. 
 //Por convenção, variáveis definidas como constantes em JavaSscript são geralmente capitalizadas e separadas por um sublinhado.
 const TAX_RATE = 0.08;//Forma de nomear a const por Convenção
+
